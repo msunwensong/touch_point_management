0b7eb3b7a0b76d51eb56a14b72b8b16e1c0a9892
sws <1321650770@qq.com>
2020-07-26 00:38:53 +0800

    Initial commit

---
+++ src/main/java/com/chinaunicom/business/development/touch_point_management/TouchPointManagementApplication.java
@@ -0,0 +1,13 @@
+package com.chinaunicom.business.development.touch_point_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchPointManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouchPointManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chinaunicom/business/development/touch_point_management/TouchPointManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinaunicom.business.development.touch_point_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouchPointManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
